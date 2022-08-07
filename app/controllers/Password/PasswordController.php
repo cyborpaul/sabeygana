48ffc2f896e4863b152e1675682e116579ca2fe8
cyborpaul <leowaldir2000@gmail.com>
2022-08-06 21:58:30 -0500

    de vsc111111111111

--- app/controllers/Password/PasswordController.php
+++ app/controllers/Password/PasswordController.php
@@ -32,8 +32,9 @@ class PasswordController extends Controller
 
         if($result->num_rows){
             $codigo=rand(pow(10, 5-1), pow(10, 5)-1);
+            $this->enviarmail($codigo, $mail);     
             $update=$this->model->updatecode($codigo, $mail);
-            $this->enviarmail($codigo, $mail);         
+                
             header('location: /sabeygana/Resetpassword');
             
             
