f2728c5397f06ee501eb1281f575f82b09bc70f3
cyborpaul <leowaldir2000@gmail.com>
2022-08-29 14:02:28 -0500

    E2121

---
+++ app/controllers/Prueba/PruebaController.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No se permite acceso directo');
+require_once ROOT . FOLDER_PATH .'/app/models/Prueba/PruebaModel.php';
+
+class PasswordController extends Controller
+{
+    public $nombre;
+
+    /**
+     * object 
+     */
+    public $model;
+    /**
+     * Inicializa valores 
+     */
+    public function __construct()
+    {
+      $this->model = new PruebaModel();
+      $this->nombre = 'Mundo';
+    }
+
+    public function exec()
+    {
+      $this->render(__CLASS__);
+    }
+
+}
+?>
---
+++ app/models/Prueba/PruebaModel.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+* 
+*/
+class PasswordnewresetModel extends Model
+{
+  
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+}
