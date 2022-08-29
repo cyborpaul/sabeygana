6f8473254457bf13d137e66b899828d90ce1497e
cyborpaul <leowaldir2000@gmail.com>
2022-08-29 14:46:23 -0500

    E2126778878

--- app/controllers/Passwordnewreset/PasswordnewresetController.php
+++ app/controllers/Passwordnewreset/PasswordnewresetController.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') or exit('No se permite acceso directo');
 require_once ROOT . FOLDER_PATH .'/app/models/Passwordnewreset/PasswordnewresetModel.php';
-
+require_once LIBS_ROUTE .'Session.php';
+require_once ROOT . FOLDER_PATH .'/app/models/Prueba/PruebaModel.php';
 class PasswordnewresetController extends Controller
 {
     public $nombre;
@@ -10,6 +11,7 @@ class PasswordnewresetController extends Controller
      * object 
      */
     public $model;
+    private $session;
     /**
      * Inicializa valores 
      */
@@ -17,15 +19,23 @@ class PasswordnewresetController extends Controller
     {
       $this->model = new PasswordnewresetModel();
       $this->nombre = 'Mundo';
+      $this->session = new Session(); 
+      $this->session->init();
+      if($this->session->getStatus() === 1| empty($this->session->get('email')))
+        exit('Acceso denegado');
     }
 
     public function exec()
     {
           $this->render(__CLASS__);
+          $params = array(
+            'email' => $this->session->get('email')
+          );
+        $this->render(__CLASS__, $params);
     }
 
     public function newpassword($request_params){
-        $mail="";
+        $mail=$this->session->get('email');;
         $password=$request_params['password'];
         $passwordre=$request_params['passwordre'];
         if($password==$passwordre){
